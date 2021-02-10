4f47f0891411670daef981ad491cc9fa6b6e672b
DarioCozzuto <77857380+DarioCozzuto@users.noreply.github.com>
2021-02-10 20:48:53 +0100

    Add files via upload

---
+++ post.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<body>
+<form name="form" action="<?php $_SERVER["PHP_SELF"];?>" method="post">
+    <input type="text" name="id" id="id" placeholder="id">
+    <input type="text" name="email" id="email" placeholder="email">
+    <input type="submit" name="submit" onclick="myFunction()">
+    </form>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $id = $_REQUEST['id'];
+    $email = $_REQUEST['email'];
+    if (empty($id) || empty($email)) {
+        echo "Riempire i campi";
+    } else {
+      echo "id=".$id;
+      echo "<br>";
+      echo "email=".$email;
+    }
+  }
+?>
+ 
+</body>
+</html>
+
